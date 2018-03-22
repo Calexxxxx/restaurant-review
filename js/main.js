b207f83fdcb7198e314c0c5e3f5865f8d851dbde
Calexxxxx <pasm28@hotmail.nl>
2018-03-22 20:51:37 +0100

    Change h1 restaurant name to h2

--- js/main.js
+++ js/main.js
@@ -142,15 +142,17 @@ createRestaurantHTML = ( restaurant ) => {
     const image = document.createElement( 'img' );
     image.className = 'restaurant-img';
 
+    // aria label
     image.setAttribute( 'aria-label', `Image of the ${restaurant.name} restaurant in ${restaurant.neighborhood} on ${restaurant.address}` );
 
+    // alt text
     image.setAttribute( 'alt', `Image of the ${restaurant.name} restaurant in ${restaurant.neighborhood} on ${restaurant.address}` );
 
     image.src = DBHelper.imageUrlForRestaurant( restaurant );
     li.append( image );
 
     li.append( div );
-    const name = document.createElement( 'h1' );
+    const name = document.createElement( 'h2' );
     name.innerHTML = restaurant.name;
     div.append( name );
 
