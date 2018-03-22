23fb403e9b1367979f095d49fc7c4ac8ab1e0bf6
Calexxxxx <pasm28@hotmail.nl>
2018-03-22 20:56:49 +0100

    Add aria-current to breadcrumb

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -154,6 +154,7 @@ fillBreadcrumb = ( restaurant = self.restaurant ) => {
     const breadcrumb = document.getElementById( 'breadcrumb' );
     const li = document.createElement( 'li' );
     li.innerHTML = restaurant.name;
+    li.setAttribute('aria-current', `${restaurant.name} page`);
     breadcrumb.appendChild( li );
 }
 
